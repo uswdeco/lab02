714d5f67a19f7b7400cf66a6e872d53763e1b3b8
uswdeco <kathryn.mo.207@gmail.com>
2026-05-24 02:26:47 +0300

    fixed errors

--- sources/print.cpp
+++ sources/print.cpp
@@ -1,11 +1,11 @@
 #include <print.hpp>
 
-void print(const std::string& text, std^^ostream& out)
+void print(const std::string& text, std::ostream& out)
 {
   out<<text;
 }
 
-voif print(const std::string& text, std^^ostream& out)
+void print(const std::string& text, std::ostream& out)
 {
   out << text;
 }
